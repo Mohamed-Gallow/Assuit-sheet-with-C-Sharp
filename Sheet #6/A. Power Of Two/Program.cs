61fd487b75124fb398046f8219ce59e58e12f6ed
Mohamed Gallow <medogallo7@gmail.com>
2025-06-29 10:06:47 +0300

    Add files via upload

---
+++ Sheet #6/A. Power Of Two/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A.Power_Of_Two
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+           
+            if (n>0 && (n & (n-1)) == 0)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+        }
+    }
+}
---
+++ Sheet #6/B. Prime checking/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B.Prime_checking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            int flag = 0;
+            for(int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if(n % i == 0)
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 1 || n == 1)
+                Console.WriteLine("NO");
+            else
+                Console.WriteLine("YES");
+        }
+    }
+}
